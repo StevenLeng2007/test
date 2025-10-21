f63e1853adbb5b6f3f75dbaecc43dce9f0307b8a
StevenLeng2007 <13761697179@139.com>
2025-10-21 09:56:38 +0800

    assignment1

---
+++ test/STM32 week2 debounce/MDK-ARM/RTE/_STM32_week2_debounce/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32 week2 debounce' 
+ * Target:  'STM32 week2 debounce' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
